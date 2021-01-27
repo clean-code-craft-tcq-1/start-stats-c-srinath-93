42b821315a3ac91cf21968b8ef5f7f562c52c166
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-01-27 23:55:35 +0530

    Update alerts.c
    trial 7 Test case 3

--- alerts.c
+++ alerts.c
@@ -3,13 +3,20 @@
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
+  int index =0;
+  int func_call_nos = 2;
   /* checking if the max and threshold is breached */
   if(computedStats.max >  maxThreshold)
   {
-    /* calling email alert function when max threshold is breached */
-    emailAlerter();
-    /* calling led alert function when max threshold is breached */
-    ledAlerter();
+    for(;index<func_call_nos;index++)
+    {
+      /* calling email and led alert function when max threshold is breached */
+      alerters[index];
+    }
+  }
+  else
+  {
+    /* do nothing */
   }
 }
 
